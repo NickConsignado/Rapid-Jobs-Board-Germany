b1d4615aa6cbaf30fad971ba875bdfc496dea256
Nick Consignado <nickconsignado@gmail.com>
2023-07-26 15:50:12 +0800

    edit class to className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,36 +8,36 @@ function offcanvas() {
 
 <header>
 
-<nav class="navbar navbar-expand-lg bg-light">
+<nav className="navbar navbar-expand-lg bg-light">
 
-  <div class="container-fluid">
-    <p class="navbar-brand"><img src={mainLogo} alt="Logo" style={{width:"200px", height:"80px"}} /></p>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+  <div className="container-fluid">
+    <p className="navbar-brand"><img src={mainLogo} alt="Logo" style={{width:"200px", height:"80px"}} /></p>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="nav nav-tabs collapse navbar-collapse " id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-        <Link to="./"><span  class="nav-link" aria-current="page" >Home</span></Link>
+    <div className="nav nav-tabs collapse navbar-collapse " id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+        <Link to="./"><span  className="nav-link" aria-current="page" >Home</span></Link>
         </li>
-        <li class="nav-item">
-        <Link to="/about"><span class="nav-link">About</span></Link>
+        <li className="nav-item">
+        <Link to="/about"><span className="nav-link">About</span></Link>
         </li>
-        <li class="nav-item">
-        <Link to="/Jobs"><span class="nav-link" >Jobs</span></Link>
+        <li className="nav-item">
+        <Link to="/Jobs"><span className="nav-link" >Jobs</span></Link>
         </li>
-        <li class="nav-item">
-        <Link to="/Testimonial"><span class="nav-link" >Testimonial</span></Link>
+        <li className="nav-item">
+        <Link to="/Testimonial"><span className="nav-link" >Testimonial</span></Link>
         </li>
-        <li class="nav-item">
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-primary" type="submit">Search</button>
+        <li className="nav-item">
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-primary" type="submit">Search</button>
           </form>
         </li>
       </ul>
-          <div class="container me-lg-0 col-lg-3">                  
+          <div className="container me-lg-0 col-lg-3">                  
           <div id="login-form" className="d-flex ">
                     <h5>Login:</h5>
                     <input className="form-control me-2 ms-2" type="text" name="username" id="username" placeholder="Username" aria-label="Username"/>
