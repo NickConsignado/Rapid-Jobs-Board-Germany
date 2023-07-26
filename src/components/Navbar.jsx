70c7adf24327d53ee064b7ff409b9649fc7077f8
Nick Consignado <nickconsignado@gmail.com>
2023-07-26 15:46:09 +0800

    manual fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,44 @@
+import { Link } from 'react-router-dom';
 import mainLogo from'../images/final-logo.png';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+
 
 function offcanvas() {
   return (
 <>
-      {['xxl'].map((expand) => (
-        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#home"><img src={mainLogo} alt="Logo" style={{width:"200px", height:"80px"}} /></Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                <img src={mainLogo} alt="Logo" style={{width:"200px", height:"80px"}} />
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className=" flex-grow-1 pe-3 mt-lg-3 ">
-                  <Nav.Link href="./">Home</Nav.Link>
-                  <Nav.Link href="/about">About</Nav.Link>
-                  <Nav.Link href="/Jobs">Jobs</Nav.Link>
-                  <Nav.Link href="/Testimonial">Testimonial</Nav.Link>
-                  <Container>
-                  <Form className="d-flex col-lg-3" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-primary" type="submit">Search</button>
-                  </Form>
-                  </Container>
-                </Nav>
-                <div className="container me-lg-0 col-lg-3">                  
-                <div id="login-form" className="d-flex ">
+
+<header>
+
+<nav class="navbar navbar-expand-lg bg-light">
+
+  <div class="container-fluid">
+    <p class="navbar-brand"><img src={mainLogo} alt="Logo" style={{width:"200px", height:"80px"}} /></p>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="nav nav-tabs collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <Link to="./"><span  class="nav-link" aria-current="page" >Home</span></Link>
+        </li>
+        <li class="nav-item">
+        <Link to="/about"><span class="nav-link">About</span></Link>
+        </li>
+        <li class="nav-item">
+        <Link to="/Jobs"><span class="nav-link" >Jobs</span></Link>
+        </li>
+        <li class="nav-item">
+        <Link to="/Testimonial"><span class="nav-link" >Testimonial</span></Link>
+        </li>
+        <li class="nav-item">
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-primary" type="submit">Search</button>
+          </form>
+        </li>
+      </ul>
+          <div class="container me-lg-0 col-lg-3">                  
+          <div id="login-form" className="d-flex ">
                     <h5>Login:</h5>
                     <input className="form-control me-2 ms-2" type="text" name="username" id="username" placeholder="Username" aria-label="Username"/>
                     <input className="form-control me-2" type="password" name="password" id="password" placeholder="Password" aria-label="Password"/>
@@ -50,14 +50,19 @@ function offcanvas() {
                     <button onClick="logout()" className="btn btn-outline-primary ms-5" type="submit">Logout</button>
                 </div>
 
-                  <span className="d-flex justify-content-center mt-2 text-primary" id="c-Account" onClick="navigateToCreateAccount()">Create Account?</span>
-              </div>  
+                  <span className="d-flex justify-content-center mt-2 text-primary" id="c-Account">Create Account?</span>
+          </div>  
+    </div>
+  </div>  
+</nav>
+</header>
+
+              
+                
+             
       
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+
+
     </>
            
 
